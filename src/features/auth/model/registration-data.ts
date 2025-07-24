8237949d838b175475275ba35c4608c8e8357a8e
Mechanikum <onemoremechmail@gmail.com>
2025-07-24 11:05:28 +0700

    chore: Added readme on how to run the project fix: Gave registration data proper type

--- src/features/auth/model/registration-data.ts
+++ src/features/auth/model/registration-data.ts
@@ -1,3 +1,7 @@
-import type { UserData } from "@/shared/model/user";
+import type { z } from "zod";
+import type UserSchema from "@/shared/model/user";
 
-export type RegistrationData = Omit<UserData, "id" | "verified">;
+export type RegistrationData = Omit<
+	z.infer<typeof UserSchema>,
+	"id" | "verified"
+>;
